2ecf03736578c5b85258c25c6a99fb5395fc8472
yurukute <nguyenkhanhdung.9999@gmail.com>
2021-12-31 04:17:35 +0700

    init

---
+++ 3_in_1_robot.ino
@@ -0,0 +1,201 @@
+#include <Servo.h>
+#include <NewPing.h>  			// Ultrasonic sensor
+#include <IRremote.h> 
+#include <SoftwareSerial.h> // Bluetooth communication
+
+// Directions
+#define STRAIGHT 100
+#define LEFT     180
+#define RIGHT    10
+// IRremote's buttons
+#define ON    0xFFA25D
+#define OFF   0xFFE21D
+#define MODE1 0xFFE01F
+#define MODE2 0xFFA857
+#define MODE3 0xFF906F
+#define SOS   0xFF18E7
+#define HOLD  0xFFFFFFFF
+// Pins
+#define RX   0
+#define TX   1
+#define ECHO 2
+#define TRIG 3
+
+SoftwareSerial BTserial(RX, TX); 
+
+NewPing sonar(TRIG, ECHO, 250);
+
+Servo servo;
+
+IRrecv irrecv(9);
+decode_results res;
+
+const int motors[] = {7,6,5,4}; //L298N pins
+int distance = 100, mode = 0;
+char last_command;
+
+void setup() {
+  for(int i : motors) pinMode(i, OUTPUT);
+  servo.attach(8);
+  servo.write(STRAIGHT);
+  irrecv.enableIRIn();
+  Serial.begin(9600);
+}
+
+void loop() {
+	if(mode != 3 && irrecv.decode(&res)){
+    switchMode(res.value);
+    irrecv.resume();
+  }
+  if(mode != 2 && Serial.available()){		
+		switchMode(Serial.read());    
+  }
+  switch(mode){
+  case 0: // Stanby mode
+    stop();
+    servo.write(STRAIGHT);
+    break;
+  case 1: obstacleAvoiding();			break;
+  case 2: bluetoothControlling();	break;		
+  case 3: irControlling();				break;
+	}
+}
+
+void switchMode(int data){
+  switch(data){
+  case ON:
+    if(mode == 0)
+      mode = 1;
+    break;
+  case OFF: case '4':
+		mode = 0;
+    break;
+  case MODE1: case '1':
+    mode = 1; 
+    break;
+  case MODE2: case '2':
+    mode = 2;
+		Serial.read();
+    break;
+  case MODE3: case '3':
+    mode = 3;
+    break;    
+  } 
+}
+
+void obstacleAvoiding(){
+	if(distance < 30){
+		stop();
+		delay(300);
+		moveBackward();
+		delay(400);
+		stop();
+		delay(300);
+		getDistance(LEFT) > getDistance(RIGHT) ? turnLeft() : turnRight();
+		delay(400);
+		stop();
+	}
+	else moveForward(); 
+	distance = getDistance(STRAIGHT);
+}
+
+int getDistance(int DIRECTION){
+  servo.write(DIRECTION);
+  delay(70);
+  int newDistance = sonar.ping_cm();
+  if(DIRECTION != STRAIGHT){
+    delay(500);
+    servo.write(STRAIGHT);
+  }
+  return newDistance == 0 ? 250 : newDistance;  
+}
+
+void bluetoothControlling(){
+	if(Serial.available()){
+		switch(Serial.read()){
+		case '1':
+			switchMode(OFF);
+			Serial.read();
+			break;
+		case '2': servo.write(RIGHT);     break;      
+		case '3': servo.write(STRAIGHT);  break;
+		case '4': servo.write(LEFT);		 	break;
+		case 'N': moveForward();          break;
+		case 'S': moveBackward();         break;
+		case 'W': turnLeft();             break;
+		case 'E': turnRight();            break;
+		}
+	}
+	delay(60);
+	stop();
+}
+
+void irControlling(){
+	if(irrecv.decode(&res)){			
+		translateIR(res.value);
+		irrecv.resume();
+	}
+	delay(180); 
+	stop();
+}
+
+void translateIR(int data){
+	switch(data){
+	case OFF:
+		switchMode(OFF);
+		break;
+	case MODE1: case 'L':
+		turnLeft();
+		last_command = 'L';
+		break;
+	case MODE2: case 'F':
+		moveForward();
+		last_command = 'F';
+		break;			 
+	case MODE3: case 'R':
+		turnRight();
+		last_command = 'R';
+		break;					
+	case SOS: case 'B':
+		moveBackward();
+		last_command = 'B';
+		break;
+	case HOLD:
+		translateIR(last_command);
+		break;
+	}
+}
+
+void stop(){
+  for(int i : motors){
+    digitalWrite(i, LOW);
+  }
+}
+
+void moveForward(){
+  digitalWrite(motors[0], HIGH); //IN1
+  digitalWrite(motors[1], LOW);  //IN2
+  digitalWrite(motors[2], HIGH); //IN3
+  digitalWrite(motors[3], LOW);  //IN4
+}
+
+void moveBackward(){
+  digitalWrite(motors[0], LOW);
+  digitalWrite(motors[1], HIGH);
+  digitalWrite(motors[2], LOW);
+  digitalWrite(motors[3], HIGH);
+}
+
+void turnLeft(){
+  digitalWrite(motors[0], HIGH);
+  digitalWrite(motors[1], LOW);
+  digitalWrite(motors[2], LOW);
+  digitalWrite(motors[3], HIGH); 
+}
+
+void turnRight(){
+	digitalWrite(motors[0], LOW);
+	digitalWrite(motors[1], HIGH);
+	digitalWrite(motors[2], HIGH);
+	digitalWrite(motors[3], LOW);  
+}
---
+++ readme.md
@@ -0,0 +1,77 @@
+# 3-IN-1 Robotic Car with Arduino Nano
+<p align="center">
+  <img src="images/cover.jpg"/>
+</p>
+## Introduction
+### About this project
+My first Arduino project. This is a four-wheel car with 3 modes:
+- Auto: Obstacle avoiding
+- Manual: Bluetooth controlling
+- Manual: Infrared controlling
+### Feature
+Unlike many tutorials on the internet, I build my robot to stop automatically without having to press any `stop button`. If I want the robot to run, I'll just hold the button on the controller and release it when i dont want to.
+### DEMO
+#### Obstacle Avoiding
+<p align="center">
+  <img src="images/DEMO/obstacle_avoiding.gif"/>
+</p>
+#### Bluetooth Controlling
+<p align="center">
+  <img src="images/DEMO/"/>
+</p>
+#### IR Controlling
+<p align="center">
+  <img src="images/DEMO/IR_controlling.gif"/>
+</p>
+## Components and Supplies
+| Image                           | Name                        | QTY |
+|---------------------------------|-----------------------------|-----|
+| ![](images/components/arduino_nano.png)    | Arduino Nano                | 1   |
+| ![](images/components/L298N.png)           | L298N - Motor Driver        | 1   |
+| ![](images/components/DC_motor.png)        | 3-6V DC motor and wheel     | 4   |
+| ![](images/components/SG90.png)            | SG90 - Micro Servo Motor    | 1   |
+| ![](images/components/HC_SR04.png)         | HC-SR04 - Ultrasonic Sensor | 1   |
+| ![](images/components/HC_06.png)           | HC-06 - Bluetooth module    | 1   |
+| ![](images/components/KY_022.png)          | KY-022 - IR Receiver Sensor | 1   |
+| ![](images/components/mini_breadboard.png) | Mini breadboard             | 1   |
+| ![](images/components/box.png)             | Plastic box                 | 1   |
+## Necessary tools
+| Image                           | Name                    |
+|---------------------------------|-------------------------|
+| ![](images/components/engineering_kit.png) | Kid's engineering kit   |
+| ![](images/components/soldering_iron.png)  | Soldering iron          |
+| ![](images/components/glue_gun.png)        | Hot glue gun            |
+| ![](images/components/jumper_wires.png)    | Jumper wires            |
+| ![](images/components/IRremote.png)        | Infrared remote control |
+
+Notes:
+- I only use the short L-shape bracket in the engineering kit. You can buy a mounting bracket for ultrasonic sensor, which is way better :3
+- You can use any IR remote, just make sure to edit the buttons' hex value in the code before uploading it to Arduino. I use the remote shown above and its hex codes are:
+```c
+#define ON    0xFFA25D
+#define OFF   0xFFE21D
+#define MODE1 0xFFE01F
+#define MODE2 0xFFA857
+#define MODE3 0xFF906F
+#define SOS	  0xFF18E7
+#define HOLD  0xFFFFFFFF // Generally, remote will send this value when you're holding a button
+```
+## App
+- Arduino IDE
+- Any Arduino bluetooth controller app. 
+
+  I chose [Arduino Bluetooth Controlled Joystick](https://play.google.com/store/apps/details?id=uncia.robotics.joystick). Besides two joystick modes and one accelerometer mode, this app provides `Button Mode` - the mode i'm using in this project. I use the default setting in the app:
+  
+  | Button       | Character send |
+  |--------------|----------------|
+  | Up arrow     | N              |
+  | Down arrow   | S              |
+  | Left arrow   | W              |
+  | Right arrow  | E              |
+  | Cross    (X) | 1              |
+  | Circle   (O) | 2              |
+  | Triangle (Δ) | 3              |
+  | Square   (□) | 4              |
+
+## Schematic
+![](images/3_in_1_robot_bb.svg)
